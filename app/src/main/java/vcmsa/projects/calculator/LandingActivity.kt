fc3f54e0eb9c1d6af1815eb84f5dfa48850ff044
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2025-02-23 14:59:32 +0200

    Changes

--- app/src/main/java/vcmsa/projects/calculator/LandingActivity.kt
+++ app/src/main/java/vcmsa/projects/calculator/LandingActivity.kt
@@ -3,6 +3,7 @@ package vcmsa.projects.calculator
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LandingActivity : AppCompatActivity() {
@@ -11,10 +12,15 @@ class LandingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_landing)
 
         val btnStart = findViewById<Button>(R.id.btnStart)
+        val btnComingSoon = findViewById<Button>(R.id.btnComingSoon)
 
         btnStart.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        btnComingSoon.setOnClickListener {
+            Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/vcmsa/projects/calculator/ResultActivity.kt
+++ app/src/main/java/vcmsa/projects/calculator/ResultActivity.kt
@@ -1,10 +1,14 @@
 package vcmsa.projects.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
+    val btnBack: Button = findViewById(R.id.btnBack)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
@@ -13,5 +17,10 @@ class ResultActivity : AppCompatActivity() {
         val result = intent.getIntExtra("result", 0)
         val resultTextView: TextView = findViewById(R.id.resultTextView)
         resultTextView.text = "Result: $result"
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
